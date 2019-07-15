99d07d6a8546dadbbf0523f14a05434e896f4fe5
hejq <603640909@qq.com>
2019-07-15 08:55:53 +0800

    数据库连接错误处理

--- src/main/java/quartz/task/config/properties/DruidProperties.java
+++ src/main/java/quartz/task/config/properties/DruidProperties.java
@@ -69,7 +69,8 @@ public class DruidProperties {
          */
         datasource.setValidationQuery(validationQuery);
 
-        /* 建议配置为true，不影响性能，并且保证安全性。申请连接的时候检测，
+        /*
+         * 建议配置为true，不影响性能，并且保证安全性。申请连接的时候检测，
          * 如果空闲时间大于timeBetweenEvictionRunsMillis，执行validationQuery检测连接是否有效。
          */
         datasource.setTestWhileIdle(testWhileIdle);
